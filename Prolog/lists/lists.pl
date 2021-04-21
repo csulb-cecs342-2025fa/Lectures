f2606abca3f3c24ca0a6a8c3696dcaa4a2d18b50
Neal Terrell <neal.terrell@csulb.edu>
2021-04-20 21:43:46 -0700

    Update lists notes.

--- Prolog/lists/lists.pl
+++ Prolog/lists/lists.pl
@@ -7,13 +7,32 @@ mylength([], 0).
 mylength([_|T], L) :- mylength(T, S), L is S + 1.
 
 % Check if the sum of the elements of a list matches the given value.
+% int list -> int
 sum([], 0).
 sum([H|T], S) :- sum(T, D), S is D + H.
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 % mylength is not tail-recursive, so let's try making it so.
 mylength2([H|T], L) :- mylength2([H|T], L, 0). % the last param will accumulate.
-mylength2([], L, L). % base case of recursion.
+mylength2([], L, Acc) :- L = Acc.
 mylength2([_|T], L, A) :- S is A + 1, mylength2(T, L, S).
 
 
@@ -36,22 +55,33 @@ evens([_|T], E) :- evens(T, E).
 ordered([_]). % list of 1 element is ordered.
 ordered([X,Y|T]) :- X =< Y, ordered([Y|T]). % list of at least 2 elements is ordered if the first is <= second, and rest is ordered.
 
+
 % quicksort: if the given list, when sorted, is equal to S.
 quicksort([X|Xs], S) :-
+	% Quicksort: first, partition the list into the Littles (< pivot) and Bigs (> pivot).
+	% Naive QS: let the first element X be the pivot.
 	partition(Xs, X, Littles, Bigs),
+
+	% Sort the littles.
 	quicksort(Littles, Ls),
+	% Sort the bigs.
 	quicksort(Bigs, Bs),
+	% The final sorted form of the list goes:
+	% (SortedLittles), X, (SortedBigs).
 	append(Ls, [X|Bs], S).
 quicksort([], []).
 
 % partition: if the given list, when partitioned around Y, results in
 % one list of elements smaller than Y, and one list of elements larger
 % than Y.
+% First parameter: the list to partition.
+% Second: the pivot.
+% Third: the list of values smaller than the pivot.
+% Fourth: the list of values larger than the pivot.
 partition([], _, [], []).
+
+% The first element belongs in Littles if it is <= the pivot.
 partition([X|Xs], Y, [X|Ls], Bs) :- X =< Y, partition(Xs, Y, Ls, Bs).
+
+% The first element belongs in Bigs if it is > the pivot.
 partition([X|Xs], Y, Ls, [X|Bs]) :- X > Y, partition(Xs, Y, Ls, Bs).
-
-
-doubles([]).
-doubles([_]).
-doubles([X,Y|T]) :- Y is X * 2, doubles([Y|T]).
