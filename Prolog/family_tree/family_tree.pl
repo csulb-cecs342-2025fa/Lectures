d71d1c900eba8a8183816a0830dc07e8e0af0e32
Neal Terrell <neal.terrell@csulb.edu>
2021-05-10 14:49:43 -0700

    Adding aunt, ancestor, cousin.

--- Prolog/family_tree/family_tree.pl
+++ Prolog/family_tree/family_tree.pl
@@ -13,3 +13,13 @@ female(michelle).
 
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
 brother(X, Y) :- male(X), sibling(X, Y).
+
+% X is an ancestor of Y.
+ancestor(X, Y) :- parent(X, Y).
+ancestor(X, Y) :- parent(X, Z), ancestor(Z, Y).
+
+% X is the aunt of Y.
+aunt(X, Y) :- female(X), parent(Y, P), sibling(P, X).
+
+% X is the cousin of Y.
+cousin(X, Y) :- parent(X, P1), parent(Y, P2), sibling(P1, P2).
