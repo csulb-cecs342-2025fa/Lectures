42b169826909ca1514c0c3520e6cb2a02c251e77
Neal Terrell <neal.terrell@csulb.edu>
2022-02-10 20:50:32 -0800

    Interpreter lecture.

---
+++ FSharp/Interpreter/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+
+// This is a very basic math interpreter, using unions to discriminate between 
+// expression operators.
+
+// An expression is either a constant value, or an arithmetic function applied to
+// one or more operands that are expressions.
+type Expression = 
+    | Const of float
+    | Add of Expression * Expression
+    | Sub of Expression * Expression
+   
+// evaluate converts an Expression object into the floating-point number it represents.
+let rec evaluate expr =
+    match expr with 
+    | Const c -> c
+    | Add (expr1, expr2) -> (evaluate expr1) + (evaluate expr2)
+    | Sub (expr1, expr2) -> (evaluate expr1) - (evaluate expr2)
+
+// Ugly print the expression.
+let demo1 = Sub (Add (Const 10, Const 3), Const 2)
+printfn "%O" demo1
+
+// Evaluate and print the expression.
+demo1
+|> evaluate
+|> printfn "%f" // prints the result of evaluating 10 + 3 - 2
