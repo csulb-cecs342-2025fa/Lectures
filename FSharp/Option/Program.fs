ad483b3055d7ab1211ca9757aa6396c805aecd5f
Neal Terrell <nealdt@gmail.com>
2024-09-19 18:27:33 -0700

    Add another example.

--- FSharp/Option/Program.fs
+++ FSharp/Option/Program.fs
@@ -7,10 +7,11 @@
 let invalidValue = None
 let validValue = Some "Neal"
 
-// There is no "null" in F#; instead, we use Option for most use cases involving "null" in Java.
+// There is no "null" in F#; instead, we use Option for most use cases involving "null".
 
 // Usage #1: a value does not exist
-let lookupId = function
+let lookupId name =
+    match name with 
     | "Neal"  -> Some 1
     | "Karyl" -> Some 2
     | "Marco" -> Some 3
@@ -21,32 +22,45 @@ match lookupId "Michelle" with
 | Some id -> printfn "That user has id %d" id
 
 // Usage #2: error state
-let divideInts num den =
-    match den with
+let safeDivide numerator denominator =
+    match denominator with
     | 0 -> None
-    | _ -> Some (num / den)
+    | _ -> Some (numerator / denominator)
 
-match divideInts 10 0 with
+match safeDivide 10 0 with
     | None -> printfn "Could not divide those"
     | Some quotient -> printfn "The quotient was %d" quotient
-
 
 // How is this different than null?
 // Because we get compiler warnings/errors if we are given an option variable and don't
 // have a match case to deal with it.
 
-open System
+// Usage #3: optional parameters.
+let printGreeting name rank =
+    match rank with
+    | None -> printfn $"Greetings, {name}!"
+    | Some r -> printfn $"Greetings, {r} {name}!"
 
+printGreeting "Neal" None
+printGreeting "Mehrdad" (Some "Dr.")
+
+
+
+open System
 // Let's expand our math interpreter with a Log case, taking an optional base for the logarithm.
 type Expression = 
-    | Const of float
+    | Literal of float
     | Add of Expression * Expression
     | Sub of Expression * Expression
     | Log of Expression * Expression option // the base is the second argument. If None, implies base "e".
 
 let rec evaluate expr =
     match expr with 
-    | Const c -> c
+    | Literal c -> c
     | Add (expr1, expr2) -> (evaluate expr1) + (evaluate expr2)
     | Sub (expr1, expr2) -> (evaluate expr1) - (evaluate expr2)
-    
+    | Log (baseExpr, None) -> Math.Log(evaluate baseExpr)
+    | Log (baseExpr, Some exponent) -> Math.Log(evaluate baseExpr, evaluate exponent)
+
+printfn $"{evaluate (Log (Literal 10.0, None))}"
+printfn $"{evaluate (Log (Literal 10.0, Some (Literal 10.0)))}"
